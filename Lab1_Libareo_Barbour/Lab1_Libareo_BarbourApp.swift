80139fcd1eac3a6bc310ac8f528d7c0e154e13ed
Libareo13 <lebareo.saleem.barbour@gmail.com>
2026-02-27 22:31:06 -0500

    Initial Commit

---
+++ Lab1_Libareo_Barbour/Lab1_Libareo_BarbourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Libareo_BarbourApp.swift
+//  Lab1_Libareo_Barbour
+//
+//  Created by Libareo Barbour on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Libareo_BarbourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
