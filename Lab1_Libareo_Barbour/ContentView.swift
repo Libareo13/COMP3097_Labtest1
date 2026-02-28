74915bd3a2cbf9e74cde1871841c2be42c3e0bfe
Libareo13 <lebareo.saleem.barbour@gmail.com>
2026-02-28 00:31:28 -0500

    Add prime checking function

--- Lab1_Libareo_Barbour/ContentView.swift
+++ Lab1_Libareo_Barbour/ContentView.swift
@@ -5,6 +5,14 @@ struct ContentView: View {
 
     @State private var number = Int.random(in: 2...100)
 
+    func isPrime(_ n: Int) -> Bool {
+        if n < 2 { return false }
+        for i in 2...Int(Double(n).squareRoot()) {
+            if n % i == 0 { return false }
+        }
+        return true
+    }
+
     var body: some View {
         VStack(spacing: 30) {
 
@@ -12,9 +20,13 @@ struct ContentView: View {
                 .font(.system(size: 80, weight: .bold))
                 .foregroundColor(.green)
 
-            Button("Prime") {}
+            Button("Prime") {
+                print(isPrime(number))
+            }
 
-            Button("non Prime") {}
+            Button("non Prime") {
+                print(!isPrime(number))
+            }
         }
     }
 }
