2a4425e4668a2e780734a04dd4482160d91e6c09
Libareo13 <lebareo.saleem.barbour@gmail.com>
2026-02-28 00:28:56 -0500

    Add Prime and Non Prime buttons

--- Lab1_Libareo_Barbour/ContentView.swift
+++ Lab1_Libareo_Barbour/ContentView.swift
@@ -2,13 +2,23 @@
 import SwiftUI
 
 struct ContentView: View {
+
     @State private var number = Int.random(in: 2...100)
 
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
+
             Text("\(number)")
-                .font(.system(size: 72))
+                .font(.system(size: 80, weight: .bold))
                 .foregroundColor(.green)
+
+            Button("Prime") {}
+
+            Button("non Prime") {}
         }
     }
+}
+
+#Preview {
+    ContentView()
 }
