4c0cc8424f3f9e0e9f2bb126cde64b51abafb064
Libareo13 <lebareo.saleem.barbour@gmail.com>
2026-03-09 18:13:35 -0400

    Add timer state variables

--- Lab1_Libareo_Barbour/ContentView.swift
+++ Lab1_Libareo_Barbour/ContentView.swift
@@ -1,40 +1,31 @@
-
 import SwiftUI
-
 struct ContentView: View {
-
     @State private var number = Int.random(in: 1...100)
     @State private var isPrimeNumber = false
     @State private var showCorrect = false
     @State private var showWrong = false
-
     @State private var correctCount = 0
     @State private var wrongCount = 0
     @State private var attempts = 0
-
     @State private var showDialog = false
-
+    @State private var timer: Timer? = nil
+    @State private var timeRemaining = 5
     var body: some View {
         VStack(spacing: 40) {
-
             Text("\(number)")
                 .font(.system(size: 60))
-
             Button("Prime") {
                 checkAnswer(userChoice: true)
             }
-
             Button("Not Prime") {
                 checkAnswer(userChoice: false)
             }
-
             if showCorrect {
                 Image(systemName: "checkmark")
                     .resizable()
                     .frame(width: 80, height: 80)
                     .foregroundColor(.green)
             }
-
             if showWrong {
                 Image(systemName: "xmark")
                     .resizable()
@@ -54,16 +45,12 @@ struct ContentView: View {
             generateNumber()
         }
     }
-
     func generateNumber() {
         number = Int.random(in: 1...100)
         isPrimeNumber = isPrime(number)
     }
-
     func checkAnswer(userChoice: Bool) {
-
         attempts += 1
-
         if userChoice == isPrimeNumber {
             correctCount += 1
             showCorrect = true
@@ -73,7 +60,6 @@ struct ContentView: View {
             showWrong = true
             showCorrect = false
         }
-
         if attempts == 10 {
             showDialog = true
         } else {
@@ -84,14 +70,12 @@ struct ContentView: View {
             }
         }
     }
-
     func resetGame() {
         correctCount = 0
         wrongCount = 0
         attempts = 0
         generateNumber()
     }
-
     func isPrime(_ num: Int) -> Bool {
         if num < 2 { return false }
         for i in 2..<num {
